92d25b2784d69a3c7982113cd0f54b66dc63f56a
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-04-25 14:29:40 +0300

    Добавьте файлы проекта.

---
+++ SearchWordInFiles.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Для поиска слова в файлах:\n";
+
+	char searchWord[20];
+	cout << "1. Напишите искомое слово: ";
+	cin.getline(searchWord, 20);
+	cout << endl;
+	char path[253];
+	cout << "2. Укажите корневую директорию для поиска: ";
+	cin.getline(path, 253);
+	cout << endl;
+	cout << "Будем искать " << searchWord << "По пути " << path;
+
+}

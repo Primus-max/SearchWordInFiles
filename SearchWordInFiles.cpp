0551386a3b0f0edea205f937bbadbed443cc8cec
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-04-25 15:39:01 +0300

    Реализация без тестов

--- SearchWordInFiles.cpp
+++ SearchWordInFiles.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <windows.h>
+#include <filesystem>
 using namespace std;
+namespace fs = std::filesystem;
 
 struct SearchDetails
 {
@@ -8,6 +11,9 @@ struct SearchDetails
 	int repetitions = 0;
 	char fileName[20];
 };
+
+SearchDetails searchWordIntoFile(ifstream& file, const char* searchWord);
+char* getFileNameFromPath(const char* path);
 
 int main()
 {
@@ -23,6 +29,56 @@ int main()
 	cout << "2. Укажите корневую директорию для поиска: ";
 	cin.getline(path, 253);
 	cout << endl;
-	cout << "Будем искать " << searchWord << "По пути " << path;
+	cout << "Будем искать " << searchWord << "По пути " << path << endl;
 
+	if (!fs::exists(path))
+		cout << "Ошибка, директория не найдена, проверьте существует ли директория и попробуйте снова";
+
+	for (fs::directory_entry file:fs::directory_iterator(path))
+	{
+		if (!fs::is_regular_file(file.path()))
+			continue;
+
+		ifstream if_file(file.path());
+		if (!if_file.is_open())
+			continue;
+
+		SearchDetails newDetail = searchWordIntoFile(if_file, searchWord);
+
+		cout << file.path() << endl;
+	}
+
+}
+
+SearchDetails searchWordIntoFile(ifstream& file, const char* searchWord) {	
+
+	SearchDetails fileDetail;
+	char buff[1024];
+	int line = 0;
+	while (!file.eof())
+	{
+		++line;		
+		cin.getline(buff, sizeof(buff));
+
+		if (strstr(buff, searchWord)) {
+			fileDetail.line = line;
+			++fileDetail.repetitions;			
+		}
+	}
+
+	return fileDetail;
+}
+
+char* getFileNameFromPath(const char* path) {
+	if (path == NULL)
+		return NULL;
+
+	const char* lastSlash = strrchr(path, '/');
+	if (!lastSlash)
+		lastSlash = strrchr(path, '\\');
+
+	if (lastSlash)
+		return (char*)lastSlash + 1;
+
+	return NULL;
 }
